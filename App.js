a26468af40052d011e4be2f4394de3b864c1b7bf
davieduardo001 <davieduardocosta01@gmail.com>
2022-07-20 09:50:04 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+//na linha abaixo importam-se as propriedades citadas e dá-se a elas aliases (apelidos) pra ficar mais simples de usar no código.
+//a instalação da lib é feita por: npm install react-native-responsive-screen --save
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <View style={styles.container}>
+        <View style={styles.responsiveBox}>
+
+          <Text style={styles.text}>This box is always of 84.5% width and 17% height.</Text>
+
+          <Text style={styles.text}>
+            Test it by running this example repo in phones/
+            emulators with screens of various dimensions and pixel per inch (ppi).
+          </Text>
+
+        </View>
+      </View>
+    )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  responsiveBox: {
+    width: wp('80%'),
+    height: hp('17%'),
+    borderWidth: 2,
+    borderColor: 'orange',
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  text: {
+    color: 'white'
+  }
+  });
+
+export default App
